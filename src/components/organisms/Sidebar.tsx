9bf13c907b012072ffa6b5cbf5059b103d411d0f
tomohiro86 <supersonic.dps@gmail.com>
2020-04-10 23:44:50 +0900

    Get channel data from firestore

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import firebase from 'firebaseConfig';
+import firebase, { db } from 'firebaseConfig';
 import { useSelector } from 'react-redux';
 import { State } from 'store';
 import { useHistory } from 'react-router-dom';
@@ -11,7 +11,23 @@ const Sidebar: React.FC = (props) => {
   const history = useHistory();
 
   const { ...rest } = props;
+  const [isVisible, setVisible] = React.useState(false);
+  const [channels, setChannels] = React.useState<firebase.firestore.DocumentData[]>([]);
+
   const currentUser = useSelector((state: State) => state.me.payload) as firebase.User;
+  const channelsRef = db.collection('channels');
+
+  React.useEffect(() => {
+    if (!isVisible) {
+      channelsRef.get().then((querySnapshot: firebase.firestore.QuerySnapshot<firebase.firestore.DocumentData>) => {
+        querySnapshot.forEach((doc: firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>) => {
+          const data = doc.data();
+          setChannels([...channels, data]);
+        });
+      });
+      setVisible(true);
+    }
+  }, [isVisible, channelsRef, channels]);
 
   const onSignout = () => {
     firebase
@@ -23,6 +39,8 @@ const Sidebar: React.FC = (props) => {
   const onCreateChannel = () => {
     console.log('check');
   };
+
+  if (!isVisible) return null;
 
   return (
     <Styled.Wrapper mode={Mode[0]} {...rest}>
--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,7 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import 'firebase/database';
-import 'firebase/storage';
+import 'firebase/firestore';
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,5 +11,7 @@ const config = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 firebase.initializeApp(config);
+
+export const db = firebase.firestore();
 
 export default firebase;
