17c98b9a7a75eb5919708793f7e488f44610cbfc
tomohiro86 <supersonic.dps@gmail.com>
2020-04-13 13:10:58 +0900

    Add channels module

--- src/module/index.ts
+++ src/module/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { loginReducer } from 'module/login/reducer';
 import { loaderReducer } from './loader/reducer';
 import { meReducer } from './me/reducer';
+import { channelsReducer } from './channels/reducer';
 import { modalReducer } from './modal/reducer';
 
 export default combineReducers({
   login: loginReducer,
   loader: loaderReducer,
   me: meReducer,
+  channels: channelsReducer,
   modal: modalReducer,
 });
