e6d8492de0d34c48736a388e7334bd202d5f9627
tomohiro86 <supersonic.dps@gmail.com>
2020-05-24 18:48:17 +0900

    Add reset value

--- src/components/organisms/ChatForm.tsx
+++ src/components/organisms/ChatForm.tsx
@@ -1,38 +1,57 @@
 import React from 'react';
+import { ENTER_KEY_CODE } from 'utils/constants';
+
 import { Styled } from 'sc/organisms/ChatForm';
 
+interface KeyboardPressEvent extends React.KeyboardEvent {
+  key: string;
+}
+
 const ChatForm: React.FC = () => {
+  const textareaRef = React.createRef<HTMLTextAreaElement>();
   const [value, setValue] = React.useState('');
 
-  const resizeTextArea = (target: EventTarget & HTMLTextAreaElement) => {
-    target.style.height = '20px';
-    target.style.height = target.scrollHeight + 'px';
+  const resizeTextArea = (reset?: boolean) => {
+    const target = document.querySelector("textarea[name='chat']") as HTMLTextAreaElement;
+    if (reset) {
+      target.style.height = '20px';
+    } else {
+      target.style.height = value ? `${target.scrollHeight}px` : '20px';
+    }
   };
 
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(e.target.value);
-    resizeTextArea(e.target);
+    resizeTextArea(e.target.value ? false : true);
   };
 
-  const onSend = () => {
-    console.log(value);
+  const onKeyDown = (e: KeyboardPressEvent) => {
+    if (((e.ctrlKey && !e.metaKey) || (!e.ctrlKey && e.metaKey)) && e.keyCode === ENTER_KEY_CODE) {
+      onSend(value);
+    }
+  };
+
+  const onSend = (value: string) => {
     setValue('');
+    resizeTextArea(true);
   };
 
   return (
     <Styled.Wrapper>
       <Styled.ChatFormField>
         <Styled.StyleTextarea
+          ref={textareaRef}
           name="chat"
           minHeight={1 * 20 + 6}
           maxHeight={5 * 20 + 6}
           autoFocus={true}
           value={value}
           placeholder="#publicへのメッセージ"
+          onKeyDown={onKeyDown}
           onChange={onChange}
         />
         <Styled.ChatFormIcons>
-          <Styled.ChatFormIconBtn type="button" label={<Styled.StyleIconSend />} onClick={onSend} />
+          <Styled.ChatFormIconBtn type="button" label={<Styled.StyleIconSend />} onClick={() => onSend(value)} />
         </Styled.ChatFormIcons>
       </Styled.ChatFormField>
     </Styled.Wrapper>
